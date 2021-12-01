da7c4975972e60f9e2e3dc49d5e338026a3289d5
Sergio García Prado <sergio@garciparedes.me>
2021-12-01 08:18:38 +0100

    * Done with "Day 1: Sonar Sweep".

---
+++ 2021/01_sonar_sweep_part_1.rs
@@ -0,0 +1,24 @@
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
+    let depths: Vec<_> = buffer
+        .trim()
+        .split('\n')
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect();
+
+    let mut ans = 0;
+    for tuple in depths.windows(2) {
+        if tuple[0] < tuple[1] {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+
+    return Ok(());
+}
